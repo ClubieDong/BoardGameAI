1d82991f4c4b2c4d1bb1085f7852627495ddadcd
Clubie <810443053@qq.com>
2020-12-12 02:53:39 +0800

    Random player

---
+++ src/Players/RandomPlayer/RandomPlayer.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vector>
+#include <chrono>
+#include <random>
+#include <cassert>
+
+template <typename Game>
+class RandomPlayer
+{
+public:
+    using GameType = Game;
+    using Action = typename Game::Action;
+
+private:
+    const Game *_Game;
+
+public:
+    inline explicit RandomPlayer(const Game &game) : _Game(&game) {}
+    inline explicit RandomPlayer(const Game &game, const std::vector<Action> &) : _Game(&game) {}
+
+    RandomPlayer(const RandomPlayer&) = delete;
+    RandomPlayer& operator=(const RandomPlayer&) = delete;
+    RandomPlayer(RandomPlayer&&) = default;
+    RandomPlayer& operator=(RandomPlayer&&) = default;
+
+    Action operator()() const
+    {
+        std::vector<Action> acts;
+        for (Action act : *_Game)
+            acts.emplace_back(act);
+        assert(acts.size() > 0);
+        static auto seed = std::chrono::system_clock::now().time_since_epoch().count();
+        static std::default_random_engine e(seed);
+        std::uniform_int_distribution<unsigned int> random(0, acts.size() - 1);
+        return acts[random(e)];
+    }
+};
